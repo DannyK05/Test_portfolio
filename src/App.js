edb8f928d2b44a2972e9585471ee0d7e85e0013e
DannyK <olubanjok@gmail.com>
2023-08-13 23:29:04 +0100

    creating and styling Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
-import Layout from './components/Layout/index.js'
+import Layout from './components/Layout/index.js';
+import Home from './components/Home/index.js';
 
 function App() {
    return(
       <>
       <Routes>
-         <Route exact path='/' element={<Layout/>}></Route>
+         <Route exact path='/' element={<Layout/>}>
+            <Route index element={<Home/>}/>
+         </Route>
       </Routes>
       </>
    )
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from  '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons';
+// import {faLinkedin, faGithub, faYoutube} from '@fortawesome/fontawesome-common-types'
 
 const Sidebar =() => (
     <div className='nav-bar'>
@@ -25,17 +26,17 @@ const Sidebar =() => (
         <ul>
             <li>
                 <a target='blank' rel='noreferrer' href=''>
-                    <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e'/>
+                    <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
                 </a>
             </li>
             <li>
                 <a target='blank' rel='noreferrer' href=''>
-                    <FontAwesomeIcon icon={faGithub} color='#4d4d4e'/>
+                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
                 </a>
             </li>
             <li>
                 <a target='blank' rel='noreferrer' href=''>
-                    <FontAwesomeIcon icon={faYoutube} color='#4d4d4e'/>
+                    <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
                 </a>
             </li>
         </ul>
